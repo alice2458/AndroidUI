5c28d2e56ffe3d78a2ab54d80df6ac23b52d2b43
Alice2458 <18845788065@163.com>
2018-07-17 16:55:25 +0800

    m

--- app/src/main/java/com/example/alice/ui/MainActivity.java
+++ app/src/main/java/com/example/alice/ui/MainActivity.java
@@ -8,29 +8,10 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] images = new int[]{
-            R.drawable.a,
-            R.drawable.b,
-            R.drawable.c,
-            R.drawable.d,
-            R.drawable.e
-    };
-
-    int currentImg = 0;
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout main = findViewById(R.id.root);
-        final ImageView image = new ImageView(this);
-        main.addView(image);
-        image.setImageResource(images[0]);
-        image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                image.setImageResource(images[++currentImg % images.length]);
-            }
-        });
     }
 }
