fcd028bd2fb5130238196be43f37eabedcb8c872
Alice2458 <18845788065@163.com>
2018-07-17 23:14:24 +0800

    m

--- app/src/main/java/com/example/alice/ui/MainActivity.java
+++ app/src/main/java/com/example/alice/ui/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.alice.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
