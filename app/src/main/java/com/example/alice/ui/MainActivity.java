1740372542c1802effa7333ec678b7191cf78cf6
Alice2458 <18845788065@163.com>
2018-07-17 16:38:23 +0800

    m

--- app/src/main/java/com/example/alice/ui/MainActivity.java
+++ app/src/main/java/com/example/alice/ui/MainActivity.java
@@ -3,34 +3,33 @@ package com.example.alice.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Bundle savedInstanceState;
+    int[] images = new int[]{
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e
+    };
+
+    int currentImg = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout layout = new LinearLayout(this);
-        super.setContentView(layout);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        final TextView show = new TextView(this);
-        Button bn = new Button(this);
-        bn.setText("OK");
-        bn.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-        layout.addView(show);
-        layout.addView(bn);
-        bn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        LinearLayout main = findViewById(R.id.root);
+        final ImageView image = new ImageView(this);
+        main.addView(image);
+        image.setImageResource(images[0]);
+        image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                show.setText(String.format("hello %s",new Date()));
+                image.setImageResource(images[++currentImg % images.length]);
             }
         });
     }
