a646efc7e892e5af4f16a2560af3bcb1d43c134f
Alice2458 <18845788065@163.com>
2018-07-19 15:46:15 +0800

    lpp

--- app/src/main/java/com/example/alice/ui/MainActivity.java
+++ app/src/main/java/com/example/alice/ui/MainActivity.java
@@ -2,31 +2,12 @@ package com.example.alice.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.FILL_PARENT,
-                ViewGroup.LayoutParams.FILL_PARENT
-        );
-        layout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams txtParam = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.FILL_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-        TextView txt = new TextView(this);
-        txt.setLayoutParams(txtParam);
-        txt.setTextSize(20);
-        layout.addView(txt,txtParam);
-        txt.setText("Large Team,要一直走到世界尽头。");
-        super.setContentView(layout,params);
+        super.setContentView(R.layout.activity_main);
     }
 }
